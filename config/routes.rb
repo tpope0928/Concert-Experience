d1c3b9a52dd5b9fa79c2de1aa09ab61a80a7658a
Thomas_Pope <tpope0928@gmail.com>
2019-12-08 14:40:13 -0500

    added all resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  delete '/logout' => 'sessions#destroy'
+  
+  resources :concerts do
+    resources :experiences, only: [:new, :index, :create, :show]
+  end
+
+  resources :experiences, only: [:index, :show, :new, :create]
+  resources :artists
+  
+  resources :users do
+    resources :concerts, only: [:index, :show, :new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
