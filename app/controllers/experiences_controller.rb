908d461942db72ac519595b2f7e21527f0ca7b36
Thomas_Pope <tpope0928@gmail.com>
2019-12-08 17:33:55 -0500

    back to normal

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,26 +1,29 @@
 class ExperiencesController < ApplicationController
-    def new
-      #if it's nested and why find the post
-      if params[:concert_id] && @concert = Experience.find_by_id(params[:concert_id])
-        @experience = @concert.experiences.build
-      else
-        @experience = Experience.new
-      end
+  def new
+    #if it's nested and why find the post
+    @concert = Concert.find_by_id(params[:concert_id])
+    if @concert
+      @experience = @concert.experiences.build
+    else
+      @experience = Experience.new
     end
-  
-    def create
-      @experience = current_user.experiences.build(experience_params)
-      if @experience.save
-        redirect_to experiences_path
-      else
-        render :new
-      end
+  end
+
+  def create
+    @experience = current_user.experiences.build(experience_params)
+    @experience.concert_id = params[:concert_id]
+    
+    if @experience.save
+      redirect_to concert_experiences_path
+    else
+      render :new
     end
-      
-        private
-      
-        def experience_params
-          params.require(:experience).permit(:content, :rating, :concert_id)
-        end
-  
+  end
+    
+      private
+    
+      def experience_params
+        params.require(:experience).permit(:content, :rating, :concert_id)
       end
+
+    end
