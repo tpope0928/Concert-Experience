2ed4288a2a9ef66741f210568181ce657104e997
Thomas_Pope <tpope0928@gmail.com>
2019-12-14 18:32:31 -0500

    reverted rating validation for now. will add later

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,6 +3,5 @@ class Experience < ApplicationRecord
     belongs_to :concert
   
     accepts_nested_attributes_for :concert
-
-    validates :rating, :numericality: true, greater_than_or_equal_to: 0, less_than: 6
+    
   end
