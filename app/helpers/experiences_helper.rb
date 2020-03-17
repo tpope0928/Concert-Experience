54deca8f092fb9938de51361b9e23a5dc302f272
Thomas_Pope <tpope0928@gmail.com>
2020-03-17 19:59:25 -0400

    fixed nested routes read and create

--- app/helpers/experiences_helper.rb
+++ app/helpers/experiences_helper.rb
@@ -1,2 +1,9 @@
 module ExperiencesHelper
+    def concert_id_field(experience)
+        if experience.concert.nil?
+            select_tag "post[concert_id]", options_from_collection_for_select(Concert.all, :id, :date)
+        else
+            hidden_field_tag "post[concert_id]", experience.concert_id
+        end
+    end
 end
